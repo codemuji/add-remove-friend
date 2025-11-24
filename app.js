4451d5cf3ba2d9ba1ad5c4c7648002cbe27e3656
codemuji <rajatpradhan011@gmail.com>
2025-11-25 00:23:40 +0530

    1.0

---
+++ app.js
@@ -0,0 +1,11 @@
+let allBtn = document.querySelectorAll("button");
+
+allBtn.forEach(function(elem){
+  elem.addEventListener("click",function(){
+    if(elem.innerHTML === "Add Friend"){
+      elem.innerHTML = "Remove";
+    }else{
+      elem.innerHTML = "Add Friend";
+    }
+  })
+})
